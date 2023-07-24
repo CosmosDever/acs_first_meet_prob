e5188235e5c4d5336586ed53d1b14072f70bfcd8
CosmosDever <aprommhgot@gmail.com>
2023-07-24 17:05:36 +0700

    Create prob3.py

---
+++ prob3.py
@@ -0,0 +1,14 @@
+def selection_sort(numbers):
+    for # your code here
+        min_idx = i
+        for # your code here
+            if # your code here
+                min_idx = j
+
+        # your code here
+
+numbers = [17, 5, 9, 12, 2]
+
+selection_sort(numbers)
+
+print(numbers)
